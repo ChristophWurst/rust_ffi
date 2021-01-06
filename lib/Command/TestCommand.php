44ff6cc334320482efeed7b0336e8bc3905c30e6
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-01-06 18:58:37 +0100

    Add platform check via composer and in dynamic code
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Command/TestCommand.php
+++ lib/Command/TestCommand.php
@@ -15,6 +15,11 @@ class TestCommand extends Command {
 	}
 
 	public function execute(InputInterface $input, OutputInterface $output) {
+		if (!extension_loaded('ffi')) {
+			$output->writeln('<error>php ffi extension missing</error>');
+			return 1;
+		}
+
 		/** @var \FFI $def */
 		$def = \FFI::cdef(
 			"int32_t dob(int32_t n);",
